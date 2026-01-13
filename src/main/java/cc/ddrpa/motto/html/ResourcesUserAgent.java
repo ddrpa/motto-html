7d44831b0be8dff07b2ea84400bf19ae68539ec7
yufan <yufan@live.com>
2026-01-13 11:14:21 +0800

    feat 页面中存在 HTTP URL 引用资源，添加读取超时机制

--- src/main/java/cc/ddrpa/motto/html/ResourcesUserAgent.java
+++ src/main/java/cc/ddrpa/motto/html/ResourcesUserAgent.java
@@ -1,10 +1,6 @@
 package cc.ddrpa.motto.html;
 
-import static org.xhtmlrenderer.util.IOUtil.readBytes;
-
 import com.lowagie.text.Image;
-import java.io.IOException;
-import java.io.InputStream;
 import org.xhtmlrenderer.pdf.ITextFSImage;
 import org.xhtmlrenderer.pdf.ITextOutputDevice;
 import org.xhtmlrenderer.pdf.ITextUserAgent;
@@ -12,16 +8,58 @@ import org.xhtmlrenderer.resource.ImageResource;
 import org.xhtmlrenderer.util.ContentTypeDetectingInputStreamWrapper;
 import org.xhtmlrenderer.util.XRLog;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URLConnection;
+import java.util.concurrent.TimeUnit;
+
+import static org.xhtmlrenderer.util.IOUtil.readBytes;
+
 public class ResourcesUserAgent extends ITextUserAgent {
 
     private static final String RESOURCES_PREFIX = "resources://";
     private static final int RESOURCES_PREFIX_LENGTH = RESOURCES_PREFIX.length();
 
     private final int dotsPerPixel;
+    private int httpConnectionConnectTimeoutInMills = 3000;
+    private int httpConnectionReadTimeoutInMMills = 5000;
+
+    /**
+     * 设置远端资源（HTTP URL）连接超时时间，默认为 3 秒
+     *
+     * @param timeout
+     * @param timeUnit
+     * @return
+     */
+    public ResourcesUserAgent setHttpConnectTimeout(int timeout, TimeUnit timeUnit) {
+        this.httpConnectionConnectTimeoutInMills = (int) timeUnit.toMillis(timeout);
+        return this;
+    }
+
+    /**
+     * 设置远端资源（HTTP URL）读取超时时间，默认为 5 秒
+     *
+     * @param timeout
+     * @param timeUnit
+     * @return
+     */
+    public ResourcesUserAgent setHttpReadTimeout(int timeout, TimeUnit timeUnit) {
+        this.httpConnectionReadTimeoutInMMills = (int) timeUnit.toMillis(timeout);
+        return this;
+    }
 
     public ResourcesUserAgent(ITextOutputDevice outputDevice, int dotsPerPixel) {
         super(outputDevice, dotsPerPixel);
         this.dotsPerPixel = dotsPerPixel;
+    }
+
+    @Override
+    protected URLConnection onHttpConnection(HttpURLConnection origin) throws MalformedURLException, IOException {
+        origin.setConnectTimeout(this.httpConnectionConnectTimeoutInMills);
+        origin.setReadTimeout(this.httpConnectionReadTimeoutInMMills);
+        return super.onHttpConnection(origin);
     }
 
     @Override
@@ -31,14 +69,14 @@ public class ResourcesUserAgent extends ITextUserAgent {
         }
         String filePath = uriStr.substring(RESOURCES_PREFIX_LENGTH);
         try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(filePath);
-            ContentTypeDetectingInputStreamWrapper cis = new ContentTypeDetectingInputStreamWrapper(
-                is)) {
+             ContentTypeDetectingInputStreamWrapper cis = new ContentTypeDetectingInputStreamWrapper(
+                     is)) {
             Image image = Image.getInstance(readBytes(cis));
             scaleToOutputResolution(image);
             return new ImageResource(uriStr, new ITextFSImage(image));
         } catch (IOException e) {
             XRLog.exception(
-                "Can't read image file; unexpected problem for URI '" + uriStr + "'", e);
+                    "Can't read image file; unexpected problem for URI '" + uriStr + "'", e);
             return new ImageResource(uriStr, null);
         }
     }
